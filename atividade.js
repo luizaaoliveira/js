618bbefdc4aa38dd16b591340b96132a868fc667
Ana Luiza <anaaluiza.oliveira@outlook.com>
2024-10-10 11:50:29 -0300

    ana

---
+++ atividade.js
@@ -0,0 +1,26 @@
+// att1
+
+// ex2
+// let msg = "ana"
+// alert(`${msg}`)
+
+// ex3
+// let nomes = true
+// alert(`${typeof nomes}`)
+
+// ex4
+// let vlrNum = Number(3.1415)
+// alert(`${vlrNum}`)
+
+
+// atividade 3
+// let nome = 'Luiza'
+// let nomeMudado = nome.replace('Oliveira');
+// console.log(nome);
+// console.log(nomeMudado);
+
+// ativiidade 6
+
+// let series = ['obx', 'umbrella', 'lucifer', 'pll', 'the wolf']
+// console.log(series[1].toUpperCase());
+
